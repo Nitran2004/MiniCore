7497e249396bccc780a35c833871c275d68a7df9
Nitran2004 <93067795+Nitran2004@users.noreply.github.com>
2024-10-14 19:03:47 -0500

    unos cambios

--- Models/MinicoreContext.cs
+++ Models/MinicoreContext.cs
@@ -23,7 +23,7 @@ public partial class MinicoreContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=PROOS10;Database=MINICORE;Trusted_Connection=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=minicoremzp.mssql.somee.com;Database=minicoremzp;User ID=Nitran2004_SQLLogin_1;Password=1y7irhnht8;Trusted_Connection=false;MultipleActiveResultSets=true;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
